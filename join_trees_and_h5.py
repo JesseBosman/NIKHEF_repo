ef7d3ad14fb4a17b0827d24e7a9bece103dbde99
JesseBosman <thebossssssman@gmail.com>
2024-07-31 13:47:10 +0200

    check

--- join_trees_and_h5.py
+++ join_trees_and_h5.py
@@ -12,3 +12,8 @@ def join_trees_and_h5(tree_path, h5_path, output_path):
     df_h5 = pd.read_hdf(h5_path)
 
     print(len(df_tree), len(df_h5["muonscore"]))
+    pass
+
+name = "mcv7.2.gsg_anti-elec-CC_1-100GeV.km3sim.jorcarec.jsh.aanet.dst_merged.root"
+
+for h5_path in os.listdir("/data/antares/users/jwbosman/ORCA6")
